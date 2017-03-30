28cb953b501b06e5a7e557c7c56b97ec955fb579
darkquake93 <daniel.carnovale@gmail.com>
2017-03-30 19:16:02 +0100

    Fixed annoying "Database changed" error and now moving onto adding of Customers part of Workshop 5. Gotta focus on Report.docx too

---
+++ DanSearch/DanSearch/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using DanSearch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DanSearch.Controllers
+{
+
+
+    public class HomeController : Controller
+    {
+        DanDB db = new DanDB();
+        private Customer myCust = new Customer
+        {
+            CustomerID = 1,
+            Name = "Donald Trump",
+            DOB = 123,
+            Gender = "Male",
+            Address1 = "Boofhead Lane",
+            Address2 = "Hell",
+            Postcode = "HL2017T",
+            Email = "bigly@gmail.com",
+            Phone = 02072034094,
+            Mobile = 07033092039,
+            WorkLoc = "Trumps Place"
+        };
+
+        public ActionResult Index()
+        {
+            var model = db.Customers.LastOrDefault();
+            return View(myCust);
+        }
+
+        [HttpGet]
+        public ViewResult AddCust()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult AddCust(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Index", customer);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "We are a Research group, and our website helps supply information about activities you are involved in, as well as information about events, conference calls, reports and publications. Please note that administrators actively monitor this information.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "253 Develepor's Road, Kingston\n0204 930 2039";
+
+            return View();
+        }
+    }
+}
