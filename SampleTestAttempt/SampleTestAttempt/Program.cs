b262a0bda29b9147c873b5a493ccd0e2161b84a4
darkquake93 <daniel.carnovale@gmail.com>
2017-05-02 23:24:11 +0100

    Sample test attempt and DanSearch

---
+++ SampleTestAttempt/SampleTestAttempt/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTestAttempt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle("Bob",-23434,10);
+            Rectangle r2 = new Rectangle("Joe",10,10);
+            Console.WriteLine(r1.getName() + "'s Area is: " + r1.Area());
+            Console.WriteLine(r2.getName() + "'s Area is: " + r2.Area());
+            Console.WriteLine("is " + r1.getName() + " a square? " + r1.isSquare());
+            Console.WriteLine("is " + r2.getName() + " a square? " + r2.isSquare());
+            if (r1.Is_Area_Greater_Than(r2))
+            {
+                Console.WriteLine(r1.getName() + "'s Area is larger than that of " + r2.getName() + "'s Area");
+            }
+            else if (r2.Is_Area_Greater_Than(r1))
+            {
+                Console.WriteLine(r2.getName() + "'s Area is larger than that of " + r1.getName() + "'s Area");
+            }
+            else
+            {
+                Console.WriteLine(r1.getName() + "'s Area is equal to " + r2.getName() + "'s Area");
+            }
+
+            // Create a list of rectangles.
+            List<Rectangle> rectangleList = new List<Rectangle>();
+
+
+            // Add rectangles to the list.
+            Console.WriteLine();
+            rectangleList.Add(new Rectangle("blardy",5,9) );
+            rectangleList.Add(new Rectangle("blardo", 34, 76));
+            rectangleList.Add(new Rectangle("rec1", 6, 4));
+            rectangleList.Add(new Rectangle("rec2", 7, 3));
+            rectangleList.Add(new Rectangle("rec3", 5, 2));
+            rectangleList.Add(new Rectangle("sq1", 4, 4));
+            rectangleList.Add(new Rectangle("sq2", 5, 5));
+
+
+
+            rectangleList.Sort();
+
+
+            Console.WriteLine("List of Rectangles After Sorting: ");
+            foreach (Rectangle r in rectangleList)
+            {
+                Console.WriteLine(r.getName() + " Area: " + r.Area());
+            }
+            
+            Console.WriteLine("Other stuff: ");
+            
+            Console.WriteLine();
+
+            double maxArea = 0;
+            String maxAreaName = "";
+            foreach (Rectangle arectangle in rectangleList)
+            {
+                if (arectangle.length == arectangle.width)
+                {
+                    Console.WriteLine(arectangle.getName() + " is a square");
+                }
+                if (arectangle.Area() > maxArea)
+                {
+                    maxArea = arectangle.Area();
+                    maxAreaName = arectangle.getName();
+                }
+                
+            }
+
+            Console.WriteLine(maxAreaName + " has largest Area of " + maxArea);
+
+           
+
+
+
+
+            Console.ReadLine();
+            //Keeps console open
+        }
+
+
+
+    }
+}
---
+++ SampleTestAttempt/SampleTestAttempt/rectangle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTestAttempt
+{
+    public class Rectangle : IComparable<Rectangle>
+    {
+        public double length { get; set; }
+        public double width { get; set; }
+        String name = "";
+
+
+        public Rectangle(String rname, double length, double width)
+        {
+            name = rname;
+            this.length = length;
+            this.width = width;
+            Console.WriteLine(rname + "'s Length is " + length);
+            Console.WriteLine(rname + "'s Width is " + width);
+        }
+        public String getName()
+        {
+            return name;
+        }
+        public double getWidth()
+        {
+            return width;
+        }
+        public double Area()
+        {
+            return length * width;
+        }
+        public String isSquare()
+        {
+
+            if (length == width)
+            {
+                return "yeah";
+            }
+            return "nah";
+        }
+        public bool Is_Area_Greater_Than(Rectangle other)
+        {
+            if (this.Area() > other.Area())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        int CompareTo(object obj)
+        {
+            int returnVal;
+            Rectangle temp = (Rectangle)obj;
+
+            if (this.Area() > temp.Area())
+                returnVal = 1;
+            else
+                if (this.Area() > temp.Area())
+                returnVal = -1;
+            else
+                returnVal = 0;
+            return returnVal;
+        }
+
+        public int CompareTo(Rectangle other)
+        {
+            if (this.Area() > other.Area())
+            {
+                return this.Area().CompareTo(other.Area());
+            }
+            return this.Area().CompareTo(other.Area());
+        }
+    }
+}
